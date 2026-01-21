a84b45ae5d9494f037b7716ba0277338c33c9c79
Solitude4sure <manojmh2025pra@gmail.com>
2026-01-21 18:10:13 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+  },
+  devIndicators: {
+    buildActivity: false,
+  },
+};
+
+export default nextConfig;
